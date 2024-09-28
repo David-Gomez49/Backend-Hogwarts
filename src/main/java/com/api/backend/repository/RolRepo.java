c17b4ede734cc2d8f72b50cec57fb9e45a93f511
DavidGomezAgudelo <david.gomez.ingeniero@gmail.com>
2024-09-28 12:02:50 -0500

    fix

--- src/main/java/com/api/backend/repository/RolRepo.java
+++ src/main/java/com/api/backend/repository/RolRepo.java
@@ -3,9 +3,12 @@ package com.api.backend.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.api.backend.model.RolModel;
 
 public interface RolRepo extends JpaRepository<RolModel, Integer>{
-    List<RolModel> findByAccessTrue();
+    
+        @Query("SELECT r FROM RolModel r WHERE r.access = TRUE")
+        List<RolModel> findByAccessTrue();
 }
