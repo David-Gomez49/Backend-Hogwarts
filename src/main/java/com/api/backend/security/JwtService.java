48afdded7c01bd76f45c4ba9bc8e146355f73791
DavidGomezAgudelo <david.gomez.ingeniero@gmail.com>
2024-09-29 13:30:54 -0500

    token

--- src/main/java/com/api/backend/security/JwtService.java
+++ src/main/java/com/api/backend/security/JwtService.java
@@ -1,9 +1,13 @@
 package com.api.backend.security;
 
+import java.sql.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Service
 public class JwtService {
@@ -11,12 +15,8 @@ public class JwtService {
     private final String SECRET_KEY = "Xx_ElPalacioDelVandolero_xX"; // Cambia esto por una clave más segura
     private final long EXPIRATION_TIME = 86400000; // 1 día
 
-    public String generateToken(String email, String name, String lastname, String picture, String role) {
+    public String generateToken(String email) {
         Map<String, Object> claims = new HashMap<>();
-        claims.put("name", name);
-        claims.put("lastname", lastname);
-        claims.put("picture", picture);
-        claims.put("role", role);
         System.out.println("----------------------------------------------------------------");
         System.out.println("SE LLAMO LA FUNCION");
         System.out.println("----------------------------------------------------------------");
@@ -28,7 +28,15 @@ public class JwtService {
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
                 */
-        System.out.println("---------------dd-d-d-d-d-d-d--d-d--d-d-");
-        return email;
+                System.out.println("---------------dd-d-d-d-d-d-d--d-d--d-d-");
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email) // Se usa el email como subject
+                .setIssuedAt(new Date(System.currentTimeMillis())) // Fecha de creación
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME)) // Fecha de expiración
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY) // Firma con clave secreta y algoritmo HS256
+                .compact();
+
+        
     }
 }
