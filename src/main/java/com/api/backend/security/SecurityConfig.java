26871f544a770a666bc2175218ed97d2188f028e
DavidGomezAgudelo <david.gomez.ingeniero@gmail.com>
2024-09-29 21:26:05 -0500

    cors (dios salvame)

--- src/main/java/com/api/backend/security/SecurityConfig.java
+++ src/main/java/com/api/backend/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -28,7 +29,9 @@ public class SecurityConfig {
                 .cors().and() // Asegurarse de habilitar CORS explícitamente
                 .csrf().disable() // Deshabilitar CSRF (opcional, depende de tu caso)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                .requestMatchers(HttpMethod.OPTIONS, "/", "/login**", "/error**", "/css/**", "/js/**", "/images/**").permitAll() // Rutas públicas
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Rutas públicas
+                .requestMatchers("/login").permitAll() // Rutas públicas
+                .requestMatchers("/rol/getPublicRoles").permitAll() // Rutas públicas
                 .anyRequest().authenticated() // Las demás rutas requieren autenticación
                 )
                 .oauth2Login(oauth2Login
@@ -41,19 +44,15 @@ public class SecurityConfig {
 
     // Definir configuración global de CORS
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Permitir solo este origen
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*")); // Permitir todos los encabezados
-        configuration.setAllowCredentials(true); // Permitir credenciales
-        configuration.addAllowedOrigin("http://localhost:5173");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:5173"); // Permitir tu frontend
+        corsConfiguration.addAllowedHeader("*"); // Permitir todos los headers
+        corsConfiguration.addAllowedMethod("*"); // Permitir todos los métodos (GET, POST, etc.)
+        corsConfiguration.setAllowCredentials(true); // Permitir cookies y autenticación
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
     }
 }
